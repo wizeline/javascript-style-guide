3e96fdb2e90d853898584dcde10ce94facaf9919
Charlie <charlie@wizeline.com>
2017-04-26 12:26:44 -0500

    arrow parens rule

--- base/index.js
+++ base/index.js
@@ -21,6 +21,7 @@ module.exports = {
     "no-param-reassign": ["error", { "props": false }],
     "quotes": ["error", "single"],
     "semi": ["error", "always"],
+    "arrow-parens": ["error", "always"],
     "strict": ["error", "never"],
     "require-yield": "off"
   }
