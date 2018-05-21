5867c97a691771e2291a012ef54152df3b73875b
Rodrigo <roguinea@gmail.com>
2018-05-20 19:35:41 -0500

    Move browser env configuration browser directory

--- base/index.js
+++ base/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: ['airbnb/base', 'prettier'],
   env: {
-    browser: true,
     node: true,
   },
   plugins: [
--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 module.exports = {
   "extends": [
     'eslint-config-wizeline',
-  ],
-  "env": {
-    "node": true,
-  },
+  ]
 };
